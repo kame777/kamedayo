09283144ecc86d141a5c55f93aee5c51694315b2
kame777 <kaikusarufa@gmail.com>
2026-02-12 20:17:51 +0900

    fix: 促進メッセージの削除

--- src/app/components/Donate.tsx
+++ src/app/components/Donate.tsx
@@ -7,7 +7,6 @@ export default function Donate() {
         src="https://cdnjs.buymeacoffee.com/1.0.0/widget.prod.min.js"
         data-id="kame777"
         data-description="Support me on Buy me a coffee!"
-        data-message="個人開発ですので、支援して頂けると大変助かります！"
         data-color="#40DCA5"
         data-position="Right"
         data-x_margin="18"
