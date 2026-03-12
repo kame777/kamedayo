6c0b9c728349f110d3c86f4bb86b0c47dcace745
kame777 <kaikusarufa@gmail.com>
2026-03-12 15:19:04 +0900

    Revert "Merge kamesuki-close-update-mainpage into main"
    This reverts commit 27ab03cdfd58fc939ae1bea3987ba74fb692c99e, reversing
    changes made to eea295e2eef5e23bc299095e993e3ea1e22841f6.

---
+++ src/app/kamesuki/page.tsx
@@ -0,0 +1,8 @@
+import KamesukiSection from '../components/KamesukiSection';
+export const metadata = {
+  title: 'かめすきー',
+};
+
+export default function KamesukiPage() {
+  return <KamesukiSection />;
+}
